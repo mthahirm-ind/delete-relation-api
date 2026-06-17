8fad576a69a65a44fd329152ee581104db992839
mthahirm-ind <m.thahirm@gmail.com>
2026-06-17 06:49:57 +0530

    Create DeleteApplication.java

---
+++ java/com/integration/DeleteApplication.java
@@ -0,0 +1,12 @@
+
+package java.com.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeleteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
